93a86824a25e79f135f8df4692b9a8d336935b8f
futa4095 <kick20000@gmail.com>
2021-07-17 13:28:29 +0900

    インデント調整

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="memo-list">
-      <MemoItem
-        v-for="(memo, index) in memos"
-        :memo="memo"
-        :key="memo.id"
-        @show="$emit('show', index)"
-      />
+    <MemoItem
+      v-for="(memo, index) in memos"
+      :memo="memo"
+      :key="memo.id"
+      @show="$emit('show', index)"
+    />
   </div>
 </template>
 
